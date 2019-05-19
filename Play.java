80e1bc6353db536d35bcc5b6c4265787cb2d44f4
BrandonZWang <42730012+BrandonZWang@users.noreply.github.com>
2019-05-19 11:14:56 -0700

    Added and set up Play

---
+++ Play.java
@@ -0,0 +1,14 @@
+
+/**
+ * The overarching class to run the game
+ *
+ * @author Brandon Wang
+ * @version May 19 2019
+ */
+public class Play
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
