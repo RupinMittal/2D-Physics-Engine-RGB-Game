3050e17e91b738354f494ebd79d5477dc0286fe9
BrandonZWang <42730012+BrandonZWang@users.noreply.github.com>
2019-05-19 11:16:20 -0700

    Added Game class

---
+++ Game.java
@@ -0,0 +1,17 @@
+
+/**
+ * Runs the game
+ *
+ * @author Brandon Wang
+ * @version May 19 2019
+ */
+public class Game
+{
+    /**
+     * Constructor for objects of class Game
+     */
+    public Game()
+    {
+        
+    }
+}
