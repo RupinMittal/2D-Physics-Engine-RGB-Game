c733849ada7bb1875c98e3ae56b07151f304465f
Jonathan Lee <jonathanlee1105@gmail.com>
2019-05-23 13:05:16 -0700

    Created constructor for music class and added the mp3 music file

---
+++ Music.java
@@ -0,0 +1,28 @@
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+
+/**
+ * The Music class will contain methods that have will allow the user to loop a music file and 
+ * then play the music
+ * @author Jonathan Lee
+ */
+public class Music 
+{
+    String fileName;    
+    
+    public Music(String file)
+    {
+        fileName = file;
+    }
+    
+    public void loop()
+    {
+
+    }
+    
+    public void play(String file)
+    {
+        
+    }
+}
