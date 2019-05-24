df42d4cf7b88c5c113a854ea0d213a6fed7a5bbb
Brandon Wang <BrandonwangHSV@gmail.com>
2019-05-24 13:36:01 -0700

    Added interaction methods to interface

--- Wall.java
+++ Wall.java
@@ -7,5 +7,31 @@
  */
 public interface Wall
 {
+    /**
+     * An interaction with the Wall when the player comes from the left
+     *
+     * @param player the Player the wall interacts with
+     */
+    public void interactLeft(Player player);
     
+    /**
+     * An interaction with the Wall when the player comes from the right
+     *
+     * @param player the Player the wall interacts with
+     */
+    public void interactRight(Player player);
+    
+    /**
+     * An interaction with the Wall when the player comes from the top
+     *
+     * @param player the Player the wall interacts with
+     */
+    public void interactFloor(Player player);
+    
+    /**
+     * An interaction with the Wall when the player comes from the bottom
+     *
+     * @param player the Player the wall interacts with
+     */
+    public void interactCeiling(Player player);
 }
