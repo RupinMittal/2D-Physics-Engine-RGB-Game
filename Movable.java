d524e320f54fa9e389532ff4cab056d3c438967e
RupinMittal6 <42721308+RupinMittal6@users.noreply.github.com>
2019-05-24 13:42:33 -0700

    Creates Movable abstract class and wrote variables.

---
+++ Movable.java
@@ -0,0 +1,14 @@
+/**
+ * Movable (abstract) - represents properties of movable objects (players and enemies)
+ * @author Rupin Mittal
+ * @version May 24, 2019
+ */
+
+public abstract class Movable
+{
+    //variables
+    private double xPos;    //the horizontal position in pixels
+    private double yPos;    //the vertical position in pixels
+    private double xVel;    //the horizontal velocity
+    private double yVel;    //the vertical velocity
+}
