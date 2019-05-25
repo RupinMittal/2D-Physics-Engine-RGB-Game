c915b7e4708b893c27e13abd6cf5bb50b7b02dee
BrandonZWang <42730012+BrandonZWang@users.noreply.github.com>
2019-05-24 23:35:37 -0700

    Implemented GreenWall methods

--- GreenWall.java
+++ GreenWall.java
@@ -24,29 +24,89 @@ public class GreenWall implements Wall
     
     /**
      * An interaction with the Wall when the player comes from the left
-     *
-     * @param player the Player the wall interacts with
+     * Sets the x velocity of the player to 0
+     * 
+     * @param futureX the theoretical x position of the player on the next frame
+     * @param futureY the theoretical y position of the player on the next frame
      */
-    public void interactLeft(Player player);
+    public void interactLeft(double futureX, double futureY)
+    {
+        //highest multiple of tileSize less than futureX - wall left edge
+        int wallX = ((int)futureX / tileSize) * tileSize; 
+        
+        //set player to touching wall
+        interactPlayer.setXPos(wallX - interactPlayer.getWidth());
+        
+        //stop player's horizontal movement
+        interactPlayer.setXVelocity(0);
+        
+        //toggle player's green status
+        interactPlayer.setOnGreenHorizontal(true);
+    }
     
     /**
      * An interaction with the Wall when the player comes from the right
-     *
-     * @param player the Player the wall interacts with
+     * Sets the x velocity of the player to 0
+     * 
+     * @param futureX the theoretical x position of the player on the next frame
+     * @param futureY the theoretical y position of the player on the next frame
      */
-    public void interactRight(Player player);
+    public void interactRight(double futureX, double futureY)
+    {
+        //lowest multiple of tileSize more than futureX - wall right edge
+        int wallX = ((int)futureX / tileSize + 1) * tileSize; 
+        
+        //set player to touching wall
+        interactPlayer.setXPos(wallX);
+        
+        //stop player's horizontal movement
+        interactPlayer.setXVelocity(0);
+        
+        //toggle player's green status
+        interactPlayer.setOnGreenHorizontal(true);
+    }
     
     /**
      * An interaction with the Wall when the player comes from the top
-     *
-     * @param player the Player the wall interacts with
+     * Sets the y velocity of the player to 0
+     * 
+     * @param futureX the theoretical x position of the player on the next frame
+     * @param futureY the theoretical y position of the player on the next frame
      */
-    public void interactFloor(Player player);
+    public void interactFloor(double futureX, double futureY)
+    {
+        //lowest multiple of tileSize less than futureY - wall top edge
+        int wallY = ((int)futureY / tileSize) * tileSize; 
+        
+        //set player to touching wall
+        interactPlayer.setXPos(wallY - interactPlayer.getHeight());
+        
+        //stop player's horizontal movement
+        interactPlayer.setYVelocity(0);
+        
+        //toggle player's green status
+        interactPlayer.setOnGreenVertical(true);
+    }
     
     /**
      * An interaction with the Wall when the player comes from the bottom
-     *
-     * @param player the Player the wall interacts with
+     * Sets the y velocity of the player to 0
+     * 
+     * @param futureX the theoretical x position of the player on the next frame
+     * @param futureY the theoretical y position of the player on the next frame
      */
-    public void interactCeiling(Player player);
+    public void interactCeiling(double futureX, double futureY)
+    {
+        //highest multiple of tileSize more than futureY - wall bottom edge
+        int wallY = ((int)futureY / tileSize + 1) * tileSize; 
+        
+        //set player to touching wall
+        interactPlayer.setXPos(wallY);
+        
+        //stop player's horizontal movement
+        interactPlayer.setYVelocity(0);
+        
+        //toggle player's green status
+        interactPlayer.setOnGreenVertical(true);
+    }
 }
