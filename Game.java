62f8529054438b30fd6fb153334fae3fb857db10
RupinMittal6 <42721308+RupinMittal6@users.noreply.github.com>
2019-05-29 13:10:40 -0700

    Wrote initializeVariables method in Game class

--- Game.java
+++ Game.java
@@ -11,7 +11,8 @@ import javafx.stage.Stage;
 public class Game extends Application
 {
     //object variables
-    //Menu menu;                                //the menu object
+    //MainMenu mainMenu;                        //the mainmenu object
+    //PauseMenu pauseMenu;                      //the pause menu
     private Environment gameEnvironment;        //the game environment
     private IntroEnvironment introEnvironment;  //the intro environment
     private Player player;                      //the player
@@ -19,17 +20,43 @@ public class Game extends Application
     private BlueWall bWall;                     //the blue wall
     private RedWall rWall;                      //the red wall
     private GreenWall gWall;                    //the green wall
-    private boolean up, down, right, left;      //the variables for the players movement
     private AnimationTimer animationTimer;      //the animation timer to run everything
 
-    //constants and movement variables
+    //variables for movement
+    private boolean up, down, right, left;      //the variables for the players movement
+    private boolean escape;                     //for escaping the game
+    private double cameraOffset;                //the variable to offset the screen for scrolling
+    private double futureXVelocity;             //the future horizontal velocity
+    private double futureYVelocity;             //the future vertical velocity
+    private double futureX;                     //future horizontal position
+    private double futureY;                     //future vertical position
+
+    //constants
     private final double Y_ACC = 7,         //the constants for movement
                          X_ACC = 7,
                          FRICT_ACC = 5,
                          GRAV_ACC = 5,
                          JUMP_ACC = 5,
                          MAX_VEL = 5;
-    private final double TILE_SIZE = 21;    //the tile size
+    private final int TILE_SIZE = 21;    //the tile size
+
+    /*
+     * Method to initialize the variables in the Game class
+     */
+    private void initializeVariables()
+    {
+        //initialize main menu
+        //initialize pause menu
+        gameEnvironment = new Environment("Sector1CollisionsData.txt", "Sector1Map.png");   //create first game environment
+        introEnvironment = new IntroEnvironment("IntroCollisionsData.txt", "IntroMap.png", "IntroForeground.png", "IntoBackground.png");    //create intro environment
+        //player = mainMenu.getPlayer;              //initialize player
+        nWall = new NormalWall(player, TILE_SIZE);  //initialize the wall variables
+        bWall = new BlueWall(player, TILE_SIZE);
+        gWall = new GreenWall(player, TILE_SIZE);
+        rWall = new RedWall(player);
+    }
+
+
 
 
 
