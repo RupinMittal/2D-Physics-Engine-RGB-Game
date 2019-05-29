7586cd7abe4a62724753484316082285729bf0ea
RupinMittal6 <42721308+RupinMittal6@users.noreply.github.com>
2019-05-29 12:26:24 -0700

    Created Game class (no code yet).

---
+++ Game.java
@@ -0,0 +1,20 @@
+/**
+ * Class to actually run the game
+ * @authors Rupin Mittal and Brandon Wang
+ * @version May 29, 2019
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
