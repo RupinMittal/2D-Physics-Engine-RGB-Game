d635c602f835eb9608a611c0dcdd3e6c3ee6e47d
Daniel Herrick <danielherrick10@yahoo.com>
2019-06-06 21:01:18 -0700

    Revert "Merge branch 'Menu' of https://github.com/APCS-4th-2018/finalproject-rgb into Menu"
    This reverts commit 28770f5089eed1196eadac204b0c90de2f262b0c, reversing
    changes made to 823a33f465275f45c817fa7ff8b82f7d955f3000.

--- RBG Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="RBG Tileset" tilewidth="75" tileheight="75" tilecount="16" columns="4">
- <image source="RBG Tileset.png" trans="ffffff" width="300" height="300"/>
-</tileset>
